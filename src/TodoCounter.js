7ee4b0e3e01d2d0c9a297e100266016acf29e56a
Orangel Barrera <obarrerafranco@gmail.com>
2022-08-19 17:50:57 +0200

    css por medio de constantes

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,8 +1,12 @@
 import React from "react";
 
+const estilos = {
+    color: 'red',
+    backgroundColor: 'yellow'
+}
 function TodoCounter(){
     return (
-        <h2>Haz Completado 2 de 3 ToDo's</h2>
+        <h2 style={estilos}>Haz Completado 2 de 3 ToDos</h2>
     )
 }
 
