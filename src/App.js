3d8eadb4b48726babd6b7b0f13aa26daa25a6086
Orangel Barrera <obarrerafranco@gmail.com>
2022-08-19 14:40:07 +0200

    Ejemplo de propiedades

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        {props.saludos}
       </header>
     </div>
   );
