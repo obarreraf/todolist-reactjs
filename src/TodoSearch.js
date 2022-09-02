5b7905428f13dba72449cbf6967f0e66bd9a94ae
Orangel Barrera <obarrerafranco@gmail.com>
2022-09-02 14:46:13 +0200

    Manejo de Estados

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,16 +2,21 @@ import React from "react";
 import './TodoSearch.css';
 
 function TodoSearch(){
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     };
 
-    return (
+
+    return [
     <input 
     className="TodoSearch" 
     placeholder="Algo" 
-    onChange={onSearchValueChange}/>    
-    )
+    onChange={onSearchValueChange}/>,
+    <p>{searchValue}</p>    
+    ];
 }
 
 export {TodoSearch};
